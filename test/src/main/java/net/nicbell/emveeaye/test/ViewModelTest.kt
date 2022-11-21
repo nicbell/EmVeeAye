a7abe6869fdb3d5939f57a7187148839a853e8ea
Nic Bell <me@nicbell.net>
2022-11-21 19:42:05 +0100

    Add testing classes.

---
+++ test/src/main/java/net/nicbell/emveeaye/test/ViewModelTest.kt
@@ -0,0 +1,41 @@
+package net.nicbell.emveeaye.test
+
+import app.cash.turbine.test
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+abstract class ViewModelTest {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get:Rule
+    val coroutinesRule = DefaultTestDispatchersRule()
+
+    suspend fun Flow<*>.assertFlow(vararg flowItems: Any) {
+        this.test {
+            flowItems.forEach {
+                Assert.assertEquals(it, awaitItem())
+            }
+        }
+    }
+
+    @ExperimentalCoroutinesApi
+    class DefaultTestDispatchersRule(
+        private val testDispatcher: TestDispatcher = StandardTestDispatcher()
+    ) : TestWatcher() {
+        override fun starting(description: Description) {
+            Dispatchers.setMain(testDispatcher)
+        }
+
+        override fun finished(description: Description) {
+            Dispatchers.resetMain()
+        }
+    }
+}
