2b5dd1ca8a1aa68bcf4037b7d5b97542df7f6f96
Nic Bell <me@nicbell.net>
2023-11-17 08:48:47 +0100

    More KDoc

--- emveeaye/src/main/java/net/nicbell/emveeaye/KClassExt.kt
+++ emveeaye/src/main/java/net/nicbell/emveeaye/KClassExt.kt
@@ -3,6 +3,7 @@ package net.nicbell.emveeaye
 import kotlin.reflect.KClass
 
 /**
- * Turns sealed class names into a useful format for logs - ParentName.ChildName
+ * Turns sealed class names into a useful format for logs
+ * @return `String` in format ParentName.ChildName
  */
 fun KClass<*>.logName() = "${this.java.superclass.simpleName}.${this.simpleName}"
--- emveeaye/src/main/java/net/nicbell/emveeaye/LifecycleExt.kt
+++ emveeaye/src/main/java/net/nicbell/emveeaye/LifecycleExt.kt
@@ -9,10 +9,22 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.launch
 
+/**
+ * Observes Flow using the `Fragment`
+ * @receiver A `Fragment` which provides viewLifecycleOwner as a `LifecycleOwner`
+ * @param flow `Flow` to observe
+ * @param collector `FlowCollector` reacting to changes
+ */
 fun <T> Fragment.observeFlow(flow: Flow<T>, collector: FlowCollector<T>) {
     viewLifecycleOwner.observeFlow(flow, collector)
 }
 
+/**
+ * Observes Flow using `LifecycleOwner`
+ * @receiver A `LifecycleOwner` so flow collection stops on lifecycle events
+ * @param flow `Flow` to observe
+ * @param collector `FlowCollector` reacting to changes
+ */
 fun <T> LifecycleOwner.observeFlow(flow: Flow<T>, collector: FlowCollector<T>) {
     lifecycleScope.launch {
         flow.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect(collector)
